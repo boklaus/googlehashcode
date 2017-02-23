ceae8a5ea86716641342c20386800f9eead7652f
zeptoTon <tony.li@zeptomind.com>
2017-02-23 19:58:57 +0000

    init commit

---
+++ googlehashcode/googlehashcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  googlehashcode
+//
+//  Created by Tony Li on 23/2/2017.
+//  Copyright © 2017 ZeptoMind Creative Ltd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
